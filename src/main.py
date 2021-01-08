72cd81503e9997d668413724351205777cb36b8a
DFXLuna <teamuba@gmail.com>
2021-01-08 10:28:15 -0700

    Registration writes to disk

--- src/main.py
+++ src/main.py
@@ -5,8 +5,12 @@ import os
 
 from discord.ext import commands
 bot = commands.Bot(command_prefix="$")
-reg = r.Registration()
-log = l.Logger(useStdOut=False)
+log = l.Logger(useStdOut=True)
+dbFilename = "db.csv"
+reg = r.Registration(dbFilename, log)
+
+def KeyFromAuthor(ctxAuthor):
+    return ctxAuthor.name + '#' + ctxAuthor.discriminator
 
 @bot.event
 async def on_ready():
@@ -18,7 +22,12 @@ async def register(ctx, teamNumber: int):
     "Register yourself to a team, removes any other registration"
     try:
         log.Log(f'register {ctx.author}')
-        reg.Register(ctx.author, teamNumber)
+        
+        val = reg.GetEntry(KeyFromAuthor(ctx.author))
+        if val is not None:
+            await ctx.send(f'{ctx.author} is already registered to {val}. Replacing that registration with {teamNumber}')
+
+        reg.Register(KeyFromAuthor(ctx.author), teamNumber)
         await ctx.send(f'Registered {ctx.author}')
     except Exception as e:
         await ctx.send('USAGE: $register [teamNumber]\ne.g. $register 1234')
@@ -29,7 +38,7 @@ async def register(ctx, teamNumber: int):
 async def unregister(ctx):
     "Remove your registration from a team"
     try:
-        reg.Unregister(ctx.author)
+        reg.Unregister(KeyFromAuthor(ctx.author))
         log.Log(f'unregister {ctx.author}')
         await ctx.send(f'unregistered {ctx.author}')
     except Exception as e:
@@ -41,7 +50,7 @@ async def check_registration(ctx):
     "Check which team you are registered to"
     log.Log(f'check_registration {ctx.author}')
     try:
-        val = reg.GetEntry(ctx.author)
+        val = reg.GetEntry(KeyFromAuthor(ctx.author))
         if val is not None:
             await ctx.send(f'{ctx.author} is registered to {val}')
         else:
@@ -53,5 +62,12 @@ async def check_registration(ctx):
 # @bot.command()
 # async def flush(ctx):
 #     reg.Flush()
+#     await ctx.send("Flushed to disk")
 
-bot.run(os.getenv('BOT_TOKEN'))
+token = os.getenv('BOT_TOKEN')
+if token == None:
+    log.LogToConsole("BOT_TOKEN environment variable not found")
+else:
+    bot.run(token)
+
+reg.Flush()
